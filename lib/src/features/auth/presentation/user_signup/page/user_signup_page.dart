0183c2984a814cc8be1f514b273e8ea70cec10df
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2024-07-23 20:54:09 +0200

    updated

--- lib/src/features/auth/presentation/user_signup/page/user_signup_page.dart
+++ lib/src/features/auth/presentation/user_signup/page/user_signup_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:journo/src/widgets/screens/app_bar_screen.dart';
+import 'package:journo/src/widgets/textfields/log_textfield_form.dart';
 
 class UserSignupPage extends StatefulWidget {
   final Function() show;
@@ -12,16 +14,26 @@ class UserSignupPage extends StatefulWidget {
 class _UserSignupPageState extends State<UserSignupPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 500,
-        width: 500,
-        color: Colors.blue,
-          child: GestureDetector(
-            child: const Text("go back"),
-            onTap: widget.show,
-          )
-      ),
+    return AppBarScreen(
+      shouldScroll: false,
+      title: "Sign Up Screen",
+      shouldHaveFloatingButton: false,
+      shouldBeCentered: true,
+      children: [
+        LongTextFieldForm(
+            onChanged: (value) {},
+            hintText: "Username",
+            labelText: "Username",
+            showSuffixIcon: false,
+            showPrefixIcon: true,
+            prefixIcon: Icons.person,
+            validator: (value) {},
+            obsureText: false),
+        TextButton(
+          onPressed: widget.show,
+          child: const Text("Already have an account?"),
+        )
+      ],
     );
   }
 }
